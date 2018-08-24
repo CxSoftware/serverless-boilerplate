ab03b0fc927e8f7984ac285f33d7eafede40a962
Juan C. Olivares <cristobal@cxsoftware.com>
2018-08-24 15:21:25 -0300

    Adding alias @

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require ('path');
 module.exports = {
 	entry: './handler.js',
 	target: 'node',
+	resolve: {
+		alias: { '@': path.resolve (__dirname, 'dist') }
+	},
 	output: {
 		libraryTarget: 'commonjs',
 		path: path.join (__dirname, '.webpack'),
